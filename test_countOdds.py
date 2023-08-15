67a006a242fe26792754c573ab2b112557d22f4c
Jane <foxfromworld@gmail.com>
2023-08-15 16:24:07 +0800

    modify test cases

--- test_countOdds.py
+++ test_countOdds.py
@@ -4,10 +4,10 @@ def test_evenL_evenH():
     assert countOdds(4,8) == 2
 
 def test_oddL_evenH():
-    assert countOdds(3,8) == 2
+    assert countOdds(3,8) == 3
 
 def test_evenL_oddH():
-    assert countOdds(4,7) == 3
+    assert countOdds(4,7) == 2
 
 def test_oddL_oddH():
-    assert countOdds(3,7) == 0
+    assert countOdds(3,7) == 3
